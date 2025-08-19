6a104ef3e226171cf05163871adb55dd09967f29
yesslee31 <yesslee31@gmail.com>
2025-08-19 15:22:59 +0900

    fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Noto Sans Korean",
-        body: "Noto Sans Korean",
+        header: "Noto Sans KR",
+        body: "Noto Sans KR",
         code: "NanumGothic Coding",
       },
       colors: {
